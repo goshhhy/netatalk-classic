257245cf48ddd9c4af272f73377a32f2b7948921
Christopher Kobayashi <software+android@disavowed.jp>
2020-08-08 10:51:17 +0900

    Indent.

--- libatalk/dsi/dsi_opensess.c
+++ libatalk/dsi/dsi_opensess.c
@@ -14,84 +14,88 @@
 #include <atalk/util.h>
 #include <atalk/logger.h>
 
-static void dsi_init_buffer(DSI *dsi)
+static void dsi_init_buffer(DSI * dsi)
 {
-    size_t quantum = dsi->server_quantum ? dsi->server_quantum : DSI_SERVQUANT_DEF;
+	size_t quantum =
+	    dsi->server_quantum ? dsi->server_quantum : DSI_SERVQUANT_DEF;
 
-    /* default is 12 * 300k = 3,6 MB (Apr 2011) */
-    if ((dsi->buffer = malloc(dsi->dsireadbuf * quantum)) == NULL) {
-        LOG(log_error, logtype_dsi, "dsi_init_buffer: OOM");
-        AFP_PANIC("OOM in dsi_init_buffer");
-    }
-    dsi->start = dsi->buffer;
-    dsi->eof = dsi->buffer;
-    dsi->end = dsi->buffer + (dsi->dsireadbuf * quantum);
+	/* default is 12 * 300k = 3,6 MB (Apr 2011) */
+	if ((dsi->buffer = malloc(dsi->dsireadbuf * quantum)) == NULL) {
+		LOG(log_error, logtype_dsi, "dsi_init_buffer: OOM");
+		AFP_PANIC("OOM in dsi_init_buffer");
+	}
+	dsi->start = dsi->buffer;
+	dsi->eof = dsi->buffer;
+	dsi->end = dsi->buffer + (dsi->dsireadbuf * quantum);
 }
 
 /* OpenSession. set up the connection */
-void dsi_opensession(DSI *dsi)
+void dsi_opensession(DSI * dsi)
 {
-  u_int32_t i = 0; /* this serves double duty. it must be 4-bytes long */
-  int offs;
-  uint8_t cmd;
-  size_t option_len;
+	u_int32_t i = 0;	/* this serves double duty. it must be 4-bytes long */
+	int offs;
+	uint8_t cmd;
+	size_t option_len;
 
-  dsi_init_buffer(dsi);
-  if (setnonblock(dsi->socket, 1) < 0) {
-      LOG(log_error, logtype_dsi, "dsi_opensession: setnonblock: %s", strerror(errno));
-      AFP_PANIC("setnonblock error");
-  }
+	dsi_init_buffer(dsi);
+	if (setnonblock(dsi->socket, 1) < 0) {
+		LOG(log_error, logtype_dsi,
+		    "dsi_opensession: setnonblock: %s", strerror(errno));
+		AFP_PANIC("setnonblock error");
+	}
 
-  /* parse options */
-  while (i + 1 < dsi->cmdlen) {
-    cmd = dsi->commands[i++];
-    option_len = dsi->commands[i++];
+	/* parse options */
+	while (i + 1 < dsi->cmdlen) {
+		cmd = dsi->commands[i++];
+		option_len = dsi->commands[i++];
 
-    if (i + option_len > dsi->cmdlen) {
-      LOG(log_error, logtype_dsi, "option %ux too large: %zu",
-          cmd, option_len);
-      exit(EXITERR_CLNT);
-    }
+		if (i + option_len > dsi->cmdlen) {
+			LOG(log_error, logtype_dsi,
+			    "option %ux too large: %zu", cmd, option_len);
+			exit(EXITERR_CLNT);
+		}
 
-    switch (cmd) {
-    case DSIOPT_ATTNQUANT:
-      if (option_len != sizeof(dsi->attn_quantum)) {
-        LOG(log_error, logtype_dsi, "option %ux bad length: %zu",
-            cmd, option_len);
-        exit(EXITERR_CLNT);
-      }
-      memcpy(&dsi->attn_quantum, &dsi->commands[i], option_len);
-      dsi->attn_quantum = ntohl(dsi->attn_quantum);
+		switch (cmd) {
+		case DSIOPT_ATTNQUANT:
+			if (option_len != sizeof(dsi->attn_quantum)) {
+				LOG(log_error, logtype_dsi,
+				    "option %ux bad length: %zu", cmd,
+				    option_len);
+				exit(EXITERR_CLNT);
+			}
+			memcpy(&dsi->attn_quantum, &dsi->commands[i],
+			       option_len);
+			dsi->attn_quantum = ntohl(dsi->attn_quantum);
 
-    case DSIOPT_SERVQUANT: /* just ignore these */
-    default:
-      break;
-    }
+		case DSIOPT_SERVQUANT:	/* just ignore these */
+		default:
+			break;
+		}
 
-    i += option_len;
-  }
+		i += option_len;
+	}
 
-  /* let the client know the server quantum. we don't use the
-   * max server quantum due to a bug in appleshare client 3.8.6. */
-  dsi->header.dsi_flags = DSIFL_REPLY;
-  dsi->header.dsi_code = 0;
-  /* dsi->header.dsi_command = DSIFUNC_OPEN;*/
+	/* let the client know the server quantum. we don't use the
+	 * max server quantum due to a bug in appleshare client 3.8.6. */
+	dsi->header.dsi_flags = DSIFL_REPLY;
+	dsi->header.dsi_code = 0;
+	/* dsi->header.dsi_command = DSIFUNC_OPEN; */
 
-  dsi->cmdlen = 2 * (2 + sizeof(i)); /* length of data. dsi_send uses it. */
+	dsi->cmdlen = 2 * (2 + sizeof(i));	/* length of data. dsi_send uses it. */
 
-  /* DSI Option Server Request Quantum */
-  dsi->commands[0] = DSIOPT_SERVQUANT;
-  dsi->commands[1] = sizeof(i);
-  i = htonl(( dsi->server_quantum < DSI_SERVQUANT_MIN || 
-	      dsi->server_quantum > DSI_SERVQUANT_MAX ) ? 
-	    DSI_SERVQUANT_DEF : dsi->server_quantum);
-  memcpy(dsi->commands + 2, &i, sizeof(i));
+	/* DSI Option Server Request Quantum */
+	dsi->commands[0] = DSIOPT_SERVQUANT;
+	dsi->commands[1] = sizeof(i);
+	i = htonl((dsi->server_quantum < DSI_SERVQUANT_MIN ||
+		   dsi->server_quantum > DSI_SERVQUANT_MAX) ?
+		  DSI_SERVQUANT_DEF : dsi->server_quantum);
+	memcpy(dsi->commands + 2, &i, sizeof(i));
 
-  /* AFP replaycache size option */
-  offs = 2 + sizeof(i);
-  dsi->commands[offs] = DSIOPT_REPLCSIZE;
-  dsi->commands[offs+1] = sizeof(i);
-  i = htonl(REPLAYCACHE_SIZE);
-  memcpy(dsi->commands + offs + 2, &i, sizeof(i));
-  dsi_send(dsi);
+	/* AFP replaycache size option */
+	offs = 2 + sizeof(i);
+	dsi->commands[offs] = DSIOPT_REPLCSIZE;
+	dsi->commands[offs + 1] = sizeof(i);
+	i = htonl(REPLAYCACHE_SIZE);
+	memcpy(dsi->commands + offs + 2, &i, sizeof(i));
+	dsi_send(dsi);
 }
