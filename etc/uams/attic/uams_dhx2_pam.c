ff410e682f442f239770519f233c2b1c7ee018fe
Christopher RYU <software+github@disavowed.jp>
2022-01-28 10:09:39 +0900

    Fully remove unused stuff.

--- etc/uams/attic/uams_dhx2_pam.c
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 1990,1993 Regents of The University of Michigan.
- * Copyright (c) 1999 Adrian Sun (asun@u.washington.edu)
- * All Rights Reserved.  See COPYRIGHT.
- */
-
-#include "config.h"
-
