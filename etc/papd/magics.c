Netatalk/netatalk

papd: Send replies to client when printing to prompt more data to be sent. Original patch by Nat Sloss.

etc/papd/magics.c
@@ -74,6 +74,7 @@ int ps( struct papfile *infile, struct papfile *outfile, struct sockaddr_at *sat
 		return( 0 );
 
 	    case -1 :
+		spoolreply( outfile, "Processing..." );
 		return( 0 );
 	    }
 
@@ -115,6 +116,7 @@ int cm_psquery( struct papfile *in, struct papfile *out, struct sockaddr_at *sat
 	    return( CH_DONE );
 
 	case -1 :
+	    spoolreply( out, "Processing..." );
 	    return( CH_MORE );
 
         case -2 :

goshhhy/netatalk-classic

etc/papd/magics.c
@@ -81,6 +81,7 @@ int ps(struct papfile *infile, struct papfile *outfile,
 				return (0);
 
 			case -1:
+                spoolreply(outfile, "Processing...");
 				return (0);
 			}
 
@@ -138,6 +139,7 @@ int cm_psquery(struct papfile *in, struct papfile *out,
 			return (CH_DONE);
 
 		case -1:
+            spoolreply(out, "Processing...");
 			return (CH_MORE);
 
 		case -2: