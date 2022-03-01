e10c3b4f88c6cb1090b4851274ecd4642d9e5dec
Christopher Kobayashi <software+github@disavowed.jp>
2022-03-01 10:01:50 +0900

    fseek() instead of fflush() -- both work, but fseek() is more correct.

--- etc/afpd/status.c
+++ etc/afpd/status.c
@@ -562,7 +562,8 @@ void set_signature(struct afp_options *options)
 				} else {
 					fseek(fp, -1L, SEEK_END);
 					if (fgetc(fp) != '\n') {
-						fflush(fp);
+						/* Required -  C11 7.21.5.3p7 */
+						fseek(fp, 0L, SEEK_END);
 						fputc('\n', fp);	/* last char is \n? */
 					}
 					goto server_signature_random;
